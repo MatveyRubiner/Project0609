fc9c51b3098e4f5be6480956fe9cb84f722ad194
MatveyRubiner <matt.rubiner@email.com>
2026-03-20 20:04:25 +0300

    fix67

--- handlers/h06_product_detail.py
+++ handlers/h06_product_detail.py
@@ -2,7 +2,7 @@ from aiogram import Router, F, Bot
 from aiogram.types import FSInputFile, CallbackQuery
 
 from bot_utils.message_utils import text_for_caption
-from database.utils import db_get_product_by_id, db_get_user_cart, db_add_or_update_item
+from database.utils import db_get_product_by_id, db_get_user_cart, db_add_or_update_item, db_get_all_categories
 from keyboards.inline import generate_category_menu, quantity_cart_controls
 from keyboards.reply import phone_kb
 
@@ -53,3 +53,23 @@ async def ask_for_phone(chat_id: int, bot: Bot):
         await bot.send_message(chat_id=chat_id,text="Предоставьте номер телефона для оформления заказа",
                                reply_markup = phone_kb())
 
+@router.callback_query(F.data =="from_detail_to_category")
+async def from_detail_to_category(callback: CallbackQuery, bot: Bot):
+    """Возврат к спискам категорий от просмотра продукта"""
+    chat_id = callback.message.chat.id
+    message_id = callback.message.message_id
+    try:
+        await bot.delete_message(chat_id=chat_id, message_id=message_id)
+    except Exception as e:
+        print(f"Сообщение для удаления не найдено,{e}")
+
+    categories=db_get_all_categories()
+
+    if not categories:
+        await bot.send_message(chat_id=chat_id,
+                               text="Категории не найдены")
+        return
+    keyboard = generate_category_menu(categories)
+    await bot.send_message(chat_id=chat_id,text="Выберите нужную вам категорию",
+                           reply_markup=keyboard)
+    await callback.answer()
